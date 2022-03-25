6acab7c0e468453eae3afb54804559b73d1bea1c
Harsh <harshvardhantiwari@Harshvardhans-MacBook-Air.local>
2022-03-25 17:36:12 +0530

    File updated

--- LeetCode/RotateList.py
+++ LeetCode/RotateList.py
@@ -6,3 +6,32 @@ def rotatelist(arr, k):
 
 
 rotatelist([1, 2, 3], 2)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        length, tail = 1, head
+
+        while(tail.next):
+            tail = tail.next
+            length += 1
+
+        k = k % length if k >= length else k
+
+        curr = head
+        for i in range(length - k - 1):
+            curr = curr.next
+        newHead = curr.next
+        curr.next = None
+        tail.next = head
+
+        return newHead
